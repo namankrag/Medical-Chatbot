7dc0de22647869635ed4e9567880019494aa5c36
Naman Kumar Agrawal <namankrag@gmail.com>
2026-05-11 22:05:44 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Naman Kumar Agrawal",
+    author_email="namankrag@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
